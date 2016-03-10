a8df6b36ccf5510db595886ed4505f3ea712d311
DataNick <DataNick@users.noreply.github.com>
2016-03-10 18:55:04 -0500

    Solutions for string problems

--- arrays.rb
+++ arrays.rb
@@ -1,1 +1,70 @@
-arrays.rb
+# Create an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+string = "A sample string to determine uniqueness of characters"
+
+# puts string.downcase.chars.uniq.count
+ #returns number of unique characters from string. In this case there are 20 unique characters
+ def count_char(string)
+  if string.length > 0 && string.chars.uniq
+    return true
+  elsif string.length < 0
+    return false
+  else
+    return false
+  end
+ end
+
+ count_char("What a wonderful world")
+
+ # 2. Given two strings, write a method to decide if one is a permutation of the other.
+
+def matching_strings(string1, string2)
+  first_string = string1.downcase.lstrip.split('').sort.join.lstrip
+  second_string = string2.downcase.lstrip.split('').sort.join.lstrip
+  first_string == second_string ? "Match" : "No Match"
+end
+
+# 3. Write a method to replace all spaces in a string with ‘%20’. For example, an
+# input of “Bitmaker Labs Summer 2013” would produce an output of
+# “Bitmaker%20Labs%20Summer%202013”.
+
+def no_space_for_spaces(string)
+  return string.gsub(' ', '%20')
+end
+
+no_space_for_spaces("Checking out this great sentence I just created!")
+
+# Implement a method to perform basic string compression using the counts of
+# consecutive repeated characters. For example, the string “abbcccccaa” would become “a1b2c5a2”. If the compressed string isn’t shorter than the original string then your method should return the original string.
+
+def compressed(string)
+  counter = Hash.new(0)
+  string.chars.each{|i|counter[i] += 1}
+  new_string = counter.to_a.join
+  if new_string > string
+    return string
+  else
+    return new_string
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
