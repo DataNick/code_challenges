3dde83b8a06778a70f96bdb742a0c62b052da5fc
DataNick <DataNick@users.noreply.github.com>
2016-03-11 00:08:34 -0500

    Additional problems and solutions for arrays

--- arrays.rb
+++ arrays.rb
@@ -1,61 +1,81 @@
-# Create an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
-string = "A sample string to determine uniqueness of characters"
+# Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+matrix = Array.new(3){Array.new(3,rand(4))}
+# puts matrix
+puts matrix.transpose.map(&:reverse)
 
-# puts string.downcase.chars.uniq.count
- #returns number of unique characters from string. In this case there are 20 unique characters
- def count_char(string)
-  if string.length > 0 && string.chars.uniq
+require 'pp'
+n = 10
+a = []
+n.times { a << (1..n).to_a }
+
+pp a
+
+0.upto(n/2-1) do |i|
+  i.upto(n-i-2) do |j|
+    tmp = a[i][j]
+    a[i][j] = a[n-j-1][i]
+    a[n-j-1][i] = a[n-i-1][n-j-1]
+    a[n-i-1][n-j-1] = a[j][n-i-1]
+    a[j][n-i-1] = tmp
+  end
+end
+
+pp a
+
+# Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
+require "spec_helper"
+
+
+class Matri
+    def initialize(input)
+        @input  = input
+        @zeros  = []
+    end
+
+    def solve
+        @input.each_with_index do |row, i|
+            row.each_with_index do |element, j|
+                @zeros << [i,j] if element == 0
+            end
+        end
+
+        @zeros.each do |x,y|
+            set_h_zero(x)
+            set_v_zero(y)
+        end
+
+        @input
+    end
+
+
+    private
+
+    def set_h_zero(row)
+        @input[row].map!{0}
+    end
+
+    def set_v_zero(col)
+        @input.size.times do |r|
+            @input[r][col] = 0
+        end
+    end
+end
+
+
+Matri.new([[1,1,1,1],[2,3,5,6], [3,0,0,1]])
+matrix = _
+matrix.solve = [[1,0,0,1],[2,0,0,6],[0,0,0,0]]
+
+# Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (e.g. “waterbottle” is a rotation of “erbottlewat”.
+
+
+def isSubString(string1, string2)
+  if string1.chars.sort == string2.chars.sort
     return true
-  elsif string.length < 0
-    return false
   else
     return false
   end
- end
-
- count_char("What a wonderful world")
-
- # 2. Given two strings, write a method to decide if one is a permutation of the other.
-
-def matching_strings(string1, string2)
-  first_string = string1.downcase.lstrip.split('').sort.join.lstrip
-  second_string = string2.downcase.lstrip.split('').sort.join.lstrip
-  first_string == second_string ? "Match" : "No Match"
 end
-
-# 3. Write a method to replace all spaces in a string with ‘%20’. For example, an
-# input of “Bitmaker Labs Summer 2013” would produce an output of
-# “Bitmaker%20Labs%20Summer%202013”.
-
-def no_space_for_spaces(string)
-  return string.gsub(' ', '%20')
-end
-
-no_space_for_spaces("Checking out this great sentence I just created!")
-
-# Implement a method to perform basic string compression using the counts of
-# consecutive repeated characters. For example, the string “abbcccccaa” would become “a1b2c5a2”. If the compressed string isn’t shorter than the original string then your method should return the original string.
-
-def compressed(string)
-  counter = Hash.new(0)
-  string.chars.each{|i|counter[i] += 1}
-  new_string = counter.to_a.join
-  if new_string > string
-    return string
-  else
-    return new_string
-  end
-end
-
-
-
-
-
-
-
-
-
-
 
 
 
